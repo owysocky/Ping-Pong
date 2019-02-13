1e3dff3bfdfd2a9b6d2ff094fb4db8ceb9aac0f5
ow <volovykolya@gmail.com>
2019-02-12 19:05:57 -0800

    bug fix, program works as expected

--- Program.cs
+++ Program.cs
@@ -2,34 +2,33 @@ using System;
 
 class Program
 {
-  public static string CheckNumber(int input)
+  public static void CheckNumber(int input)
   {
-    for (int i=0; i<=input; i++)
+    for (int i=1; i<=input; i++)
     {
       if(i % 5 == 0 && i % 3 == 0)
       {
-        return "ping-pong";
+        Console.WriteLine("ping-pong");
       }
       else if(i%3==0)
       {
-        return "ping";
+        Console.WriteLine("ping");
       }
       else if(i%5==0)
       {
-        return "pong";
+        Console.WriteLine("pong");
       }
       else
       {
-        return Convert.ToString(i);
+        Console.WriteLine(i);
       }
     }
-    return "return";
   }
   public static void Main()
   {
     Console.WriteLine("Ping-Pong");
     Console.WriteLine("Enter a word number: ");
     int userInput = int.Parse(Console.ReadLine());
-    Console.WriteLine(CheckNumber(userInput));
+    CheckNumber(userInput);
   }
 }
